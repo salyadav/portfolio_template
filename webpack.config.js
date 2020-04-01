672d39515bb43c29de9e67eecc03dd4b0836a105
salyadav <39609162+salyadav@users.noreply.github.com>
2020-04-01 18:36:18 +0530

    techmurgi/webpack configuration and basic navbar layout in header

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin }= require('clean-webpack-plugin');   
+const path = require('path');
+// const isDevelopment = process.env.NODE_ENV === 'development';
+// 
+module.exports = {
+    entry: {
+        index: './src/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules:[{
+            test: /\.html$/i,
+            loader: 'html-loader',
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }, { 
+            test: /\.s(a|c)ss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+        	]
+        }, {
+            test: /\.css$/,
+            use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|jpg|gif|svg)$/,
+            use: [
+            {
+                loader: 'file-loader'
+            }]
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.html']
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+            template: './src/index.html',
+            inject: true,
+            chunks: ['index'],
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin()
+    ], 
+    devServer: {
+        contentBase: './dist',
+        open: true
+    }
+};
